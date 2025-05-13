f8e6817b95798e324fa481c905a6b786f9ff696e
idnm <nnemkov@gmail.com>
2025-05-13 15:22:12 +0300

    plot notebook and data

--- run_bp_experiments.py
+++ run_bp_experiments.py
@@ -1,25 +1,8 @@
 from experiments import BPExperiment
 
-# qubits = list(range(2, 12+1, 2))
-# layers = list(range(5, 25+1, 5))
-# exp_bp = BPExperiment('bp_hea')
-
-# qubits = list(range(2, 12+1, 2))
-# layers = list(range(20, 25+1, 5))
-# exp_bp = BPExperiment.load('bp_hea')
-#
-#
-# exp_bp.run(
-#     qubits,
-#     layers,
-#     num_paulis=50,
-#     num_samples=50,
-#     seed=42)
-
-qubits = list(range(12, 12+1, 2))
-layers = list(range(5, 25+1, 5))
-exp_bp = BPExperiment.load('bp_hea')
-
+qubits = list(range(2, 10+1, 2))
+layers = list(range(5, 30+1, 5))
+exp_bp = BPExperiment('bp_hea')
 
 exp_bp.run(
     qubits,
@@ -27,4 +10,3 @@ exp_bp.run(
     num_paulis=50,
     num_samples=50,
     seed=42)
-
